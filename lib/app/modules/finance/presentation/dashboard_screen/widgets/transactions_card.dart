d11342a823266c4770757bd962872018250742bd
Jannik Wienecke <jannik.wienecke@sirum.de>
2023-02-26 16:12:19 +0100

    add slideable transaction card item

--- lib/app/modules/finance/presentation/dashboard_screen/widgets/transactions_card.dart
+++ lib/app/modules/finance/presentation/dashboard_screen/widgets/transactions_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:notes/app/config/constants/test_transactions.dart';
 import 'package:notes/app/modules/finance/domain/transaction.dart';
 import 'package:notes/app/modules/finance/domain/transaction_category.dart';
@@ -79,23 +80,18 @@ class TransactionList extends StatelessWidget {
           final isThisWeek = index < 3;
           final isThisMonth = index < 6;
 
-          return Padding(
-            padding: const EdgeInsets.only(
-              top: 16,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (isThisWeek && index == 0 || isThisMonth && index == 3)
-                  TransactionGroupHeader(isThisWeek: isThisWeek),
-                const SizedBox(height: 14),
-                TransactionRow(
-                  transactions: transactions,
-                  category: category,
-                  transaction: transaction,
-                ),
-              ],
-            ),
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (isThisWeek && index == 0 || isThisMonth && index == 3)
+                TransactionGroupHeader(isThisWeek: isThisWeek),
+              const SizedBox(height: 14),
+              TransactionRow(
+                transactions: transactions,
+                category: category,
+                transaction: transaction,
+              ),
+            ],
           );
         },
       ),
@@ -140,59 +136,138 @@ class TransactionRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: transaction.type == TransactionType.income
-                ? Theme.of(context).colorScheme.primaryContainer
-                : Theme.of(context).colorScheme.tertiaryContainer,
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          TransactionItemActionBtn(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            label: 'Edit',
+            isLeft: true,
+            onTab: () {
+              //
+            },
           ),
-          child: Icon(
-            category.icon,
-            size: 24,
-            color: Theme.of(context).colorScheme.onTertiaryContainer,
+          TransactionItemActionBtn(
+            color: Theme.of(context).colorScheme.errorContainer,
+            label: 'Delete',
+            isLeft: false,
+            onTab: () {
+              //
+            },
           ),
-        ),
-        const SizedBox(width: 15),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              transaction.title,
-              style: TextStyle(
-                fontSize: 15,
-                color: Theme.of(context).colorScheme.onSecondaryContainer,
-              ),
+        ],
+      ),
+      child:
+          TransactionRowContent(transaction: transaction, category: category),
+    );
+  }
+}
+
+class TransactionRowContent extends StatelessWidget {
+  const TransactionRowContent({
+    super.key,
+    required this.transaction,
+    required this.category,
+  });
+
+  final Transaction transaction;
+  final TransactionCategoery category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: transaction.type == TransactionType.income
+                  ? Theme.of(context).colorScheme.primaryContainer
+                  : Theme.of(context).colorScheme.tertiaryContainer,
             ),
-            Text(
-              category.title,
-              style: TextStyle(
-                fontSize: 12,
-                color: Theme.of(context)
-                    .colorScheme
-                    .onTertiaryContainer
-                    .withOpacity(.7),
-              ),
+            child: Icon(
+              category.icon,
+              size: 20,
+              color: Theme.of(context).colorScheme.onTertiaryContainer,
             ),
-          ],
-        ),
-        const Spacer(),
-        Text(
-          '${transaction.amount} €',
-          style: TextStyle(
-            fontSize: 15,
-            color: transaction.type == TransactionType.income
-                ? Colors.green
-                : Theme.of(context).colorScheme.onSecondaryContainer,
-            fontWeight: FontWeight.bold,
           ),
-        ),
-        const SizedBox(width: 10),
-      ],
+          const SizedBox(width: 15),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                transaction.title,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                ),
+              ),
+              Text(
+                category.title,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Theme.of(context)
+                      .colorScheme
+                      .onTertiaryContainer
+                      .withOpacity(.7),
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          Text(
+            '${transaction.amount} €',
+            style: TextStyle(
+              fontSize: 15,
+              color: transaction.type == TransactionType.income
+                  ? Colors.green
+                  : Theme.of(context).colorScheme.onSecondaryContainer,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(width: 10),
+        ],
+      ),
+    );
+  }
+}
+
+class TransactionItemActionBtn extends StatelessWidget {
+  const TransactionItemActionBtn({
+    super.key,
+    required this.color,
+    required this.label,
+    required this.isLeft,
+    required this.onTab,
+    this.flex,
+  });
+
+  final Color color;
+  final String label;
+  final bool isLeft;
+  final VoidCallback onTab;
+  final int? flex;
+
+  @override
+  Widget build(BuildContext context) {
+    return SlidableAction(
+      onPressed: (BuildContext b) {
+        //
+      },
+      flex: flex ?? 1,
+      borderRadius: BorderRadius.only(
+        topLeft: isLeft ? const Radius.circular(8) : Radius.zero,
+        topRight: isLeft ? Radius.zero : const Radius.circular(8),
+        bottomLeft: isLeft ? const Radius.circular(8) : Radius.zero,
+        bottomRight: isLeft ? Radius.zero : const Radius.circular(8),
+      ),
+      backgroundColor: color,
+      foregroundColor: Colors.white,
+      label: label,
     );
   }
 }
